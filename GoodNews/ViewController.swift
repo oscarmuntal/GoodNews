b436a49c30bee029184147d6d203f673a2348fc4
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-08 18:46:41 +0100

    Initial Commit

---
+++ GoodNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodNews
+//
+//  Created by Òscar Muntal on 8/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
