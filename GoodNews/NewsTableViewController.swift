05df9e9c749975757b94af030248b75f802be90a
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-08 19:18:16 +0100

    [NewsTableVC]: Configuring UI: NewsTableVC added with custom navigation and status bar color

---
+++ GoodNews/NewsTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewsTableViewController.swift
+//  GoodNews
+//
+//  Created by Òscar Muntal on 8/3/23.
+//
+
+import Foundation
+import UIKit
+
+
+class NewsTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+}
