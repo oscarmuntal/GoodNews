a05902e01c1c969fb21febdf8a3071ea689cc30b
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-08 20:26:42 +0100

    [NewsTableVC]: Implementation of the URLRequest observable to perform the request

---
+++ GoodNews/Extensions/URLRequest+Extensions.swift
@@ -0,0 +1,28 @@
+//
+//  URLRequest+Extensions.swift
+//  GoodNews
+//
+//  Created by Òscar Muntal on 8/3/23.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import UIKit
+
+struct Resource<T: Decodable> {
+    let url: URL
+}
+
+extension URLRequest {
+    static func requestDecodable<T>(resource: Resource<T>) -> Observable<T?> {
+        
+        return Observable.from([resource.url])
+            .flatMap { url -> Observable<Data> in
+                let request = URLRequest(url: url)
+                return URLSession.shared.rx.data(request: request)
+            }.map { data -> T? in
+                return try? JSONDecoder().decode(T.self, from: data)
+            }.asObservable()
+    }
+}
--- GoodNews/NewsTableViewController.swift
+++ GoodNews/NewsTableViewController.swift
@@ -39,15 +39,12 @@ private extension NewsTableViewController {
         let apiKey = "GET-YOUR-PRIVATE-API-KEY"
         let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=\(apiKey)")!
         
-        Observable.just(url)
-            .flatMap { url -> Observable<Data> in
-                let request = URLRequest(url: url)
-                return URLSession.shared.rx.data(request: request)
-            }.map { data -> [Article]? in
-                return try? JSONDecoder().decode(ArticlesResponse.self, from: data).articles
-            }.subscribe(onNext: { [weak self] articles in
-                if let articles = articles {
-                    self?.articles = articles
+        let resource = Resource<ArticlesResponse>(url: url)
+        
+        URLRequest.requestDecodable(resource: resource)
+            .subscribe(onNext: { [weak self] result in
+                if let result = result {
+                    self?.articles = result.articles
                     DispatchQueue.main.async {
                         self?.tableView.reloadData()
                     }
